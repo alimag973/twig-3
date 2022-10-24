fffe08043f7b6bb0ae235badf2d54ac8f170297e
Magali Perche <magali.per@hotmail.fr>
2022-10-24 10:59:31 +0200

    quete ok

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+$author = 'magali';
+$contactEmail = 'contact@wildcodeschool.com';
+
+$twig->addGlobal('author', $author);
+$twig->addGlobal('contactEmail', $contactEmail);
